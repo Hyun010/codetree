5c795c3ee5059e98ae84a476323034723d9f3e2e
Hyun010 <156050517+Hyun010@users.noreply.github.com>
2025-04-14 15:10:53 +0900

    Title: 100점 도달하기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250414/100점 도달하기/reach-100-point.py
+++ 250414/100점 도달하기/reach-100-point.py
@@ -6,7 +6,7 @@ for i in range(N,101):
         print("B", end=" ")
     elif i>=70:
         print("C", end=" ")
-    elif i>=960:
+    elif i>=60:
         print("D", end=" ")
     else:
         print("F", end=" ")
