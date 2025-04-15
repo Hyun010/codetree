85bab719bf3bcd49e882c981ebcf12f68dc3c673
Hyun010 <156050517+Hyun010@users.noreply.github.com>
2025-04-15 15:13:43 +0900

    Title: 약수가 세 개인 수, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250415/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -0,0 +1,13 @@
+def cnt(n):
+    c=1
+    for i in range(2,n+1):
+        if n%i==0:
+            c+=1
+    return c
+
+start, end = map(int, input().split())
+h=0
+for i in range(start,end):
+   if cnt(i)==3:
+    h+=1
+print(h) 
