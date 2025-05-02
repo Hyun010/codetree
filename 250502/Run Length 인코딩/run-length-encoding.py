9d70b9bd966f7c803d8cc335a186b363f226949d
Hyun010 <156050517+Hyun010@users.noreply.github.com>
2025-05-02 16:58:47 +0900

    Title: Run Length 인코딩, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250502/Run Length 인코딩/run-length-encoding.py
+++ 250502/Run Length 인코딩/run-length-encoding.py
@@ -1,15 +1,19 @@
 A = list(input())
-c=A[0]
-cnt=1
-s=''
-for i in range(1,len(A)):
-    if A[i]==c:
-        cnt+=1
-    else:
-        s+=f'{c}{cnt}'
-        c=A[i]
-        cnt=1
-    if i==len(A)-1:
-        s+=f'{c}{cnt}'
-print(len(s))
-print(s)
+if len(A)<=1:
+    print(2)
+    print(f'{A[0]}1')
+else:
+    c=A[0]
+    cnt=1
+    s=''
+    for i in range(1,len(A)):
+        if A[i]==c:
+            cnt+=1
+        else:
+            s+=f'{c}{cnt}'
+            c=A[i]
+            cnt=1
+        if i==len(A)-1:
+            s+=f'{c}{cnt}'
+    print(len(s))
+    print(s)
