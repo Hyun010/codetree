74dc3605806b01afc1123ad7821a31c234e7b873
Hyun010 <156050517+Hyun010@users.noreply.github.com>
2025-05-02 14:40:16 +0900

    Title: 파스칼의 삼각형, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250502/파스칼의 삼각형/pascals-triangle.py
@@ -0,0 +1,10 @@
+n=int(input())
+l=[[1]*n for _ in range(n)]
+for i in range(1,n):
+    for j in range(i+1):
+        if 1<=j<i:
+            l[i][j]=l[i-1][j-1]+l[i-1][j]
+for i in range(n):
+    for j in range(i+1):
+        print(l[i][j],end=' ')
+    print()
