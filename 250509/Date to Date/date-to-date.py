3ece4c095e7c8fbb65324ce4aac1dd6d0e873c1a
Hyun010 <156050517+Hyun010@users.noreply.github.com>
2025-05-09 22:16:53 +0900

    Title: Date to Date, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250509/Date to Date/date-to-date.py
+++ 250509/Date to Date/date-to-date.py
@@ -3,6 +3,8 @@ t1=0
 t2=0
 if m1==m2 and d1==d2:
     print(1)
+elif m1==m2:
+    print(d2-d1+1)
 else:
     if m1<=7:
         if m1==2:
@@ -16,18 +18,17 @@ else:
             t1=31
         else:
             t1=30
-    if m2-m1!=0:
-        for i in range(m1+1,m2):
-            if m1<=7:
-                if i==2:
-                    t2+=28
-                elif i%2==0:
-                    t2+=30
-                else:
-                    t2+=31
+    for i in range(m1+1,m2):
+        if m1<=7:
+            if i==2:
+                t2+=28
+            elif i%2==0:
+                t2+=30
             else:
-                if i%2==0:
-                    t2+=31
-                else:
-                    t2+=30
+                t2+=31
+        else:
+            if i%2==0:
+                t2+=31
+            else:
+                t2+=30
     print(t1-d1+1+d2+t2)
