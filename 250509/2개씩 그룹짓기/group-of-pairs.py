1e2c5a9e863f8041b43f6dbf56472da3cc5af13c
Hyun010 <156050517+Hyun010@users.noreply.github.com>
2025-05-09 18:20:10 +0900

    Title: 2개씩 그룹짓기, Time: 56ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250509/2개씩 그룹짓기/group-of-pairs.py
+++ 250509/2개씩 그룹짓기/group-of-pairs.py
@@ -1,4 +1,7 @@
 n = int(input())
 nums = list(map(int, input().split()))
 nums.sort()
-print(max(nums[0]+nums[-1],nums[1]+nums[-2]))
+l=[]
+for i in range(n//2+1):
+    l.append(nums[i]+nums[-(i+1)])
+print(max(l))
