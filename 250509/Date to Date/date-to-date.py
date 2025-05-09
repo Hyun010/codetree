e977330e43f10976535df0e5125759cefa5c42c6
Hyun010 <156050517+Hyun010@users.noreply.github.com>
2025-05-09 22:05:06 +0900

    Title: Date to Date, Time: 52ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250509/Date to Date/date-to-date.py
+++ 250509/Date to Date/date-to-date.py
@@ -1,13 +1,16 @@
 m1, d1, m2, d2 = map(int, input().split())
 t1=0
-if m1<=7:
-    if m1%2==0:
-        t1=30
-    else:
-        t1=31
+if m1==m2 and d1==d2:
+    print(1)
 else:
-    if m1%2==0:
-        t1=31
+    if m1<=7:
+        if m1%2==0:
+            t1=30
+        else:
+            t1=31
     else:
-        t1=30
-print(t1-d1+1+d2)
+        if m1%2==0:
+            t1=31
+        else:
+            t1=30
+    print(t1-d1+1+d2)
