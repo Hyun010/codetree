66d9edce5a910f7dc9a8ae1913f482db929dd31c
Hyun010 <156050517+Hyun010@users.noreply.github.com>
2025-05-09 22:08:32 +0900

    Title: Date to Date, Time: 72ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250509/Date to Date/date-to-date.py
+++ 250509/Date to Date/date-to-date.py
@@ -1,5 +1,6 @@
 m1, d1, m2, d2 = map(int, input().split())
 t1=0
+t2=0
 if m1==m2 and d1==d2:
     print(1)
 else:
@@ -13,4 +14,16 @@ else:
             t1=31
         else:
             t1=30
-    print(t1-d1+1+d2)
+    if m2-m1!=0:
+        for i in range(m1+1,m2):
+            if m1<=7:
+            if m1%2==0:
+                t2+=30
+            else:
+                t2+=31
+        else:
+            if m1%2==0:
+                t2+=31
+            else:
+                t2+=30
+    print(t1-d1+1+d2+t2)
